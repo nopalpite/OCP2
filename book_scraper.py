0dc785050771030ea29c8bbdff4cd94b5f6363c6
nopalpite <nopalpite@gmail.com>
2022-06-28 23:35:14 +0200

    modification book_scraper

--- book_scraper.py
+++ book_scraper.py
@@ -12,32 +12,31 @@ class Book:
         self.soup = BeautifulSoup(self.page.content, 'html.parser')
 
     def get_url(self):
-        return {"product_page_url": self.url}
+        return self.url
 
     def get_upc(self):
         upc_tag = self.soup.find("th", string="UPC")
         upc = upc_tag.find_next().string
-        return {"universal_product_code (upc)": upc}
-
+        return upc
     def get_title(self):
         title_tag = self.soup.find("h1")
         title = title_tag.string
-        return {"title": title}
+        return title
 
     def get_price(self):
         price_tag = self.soup.find("th", string="Price (incl. tax)")
         price = price_tag.find_next().string
-        return {"price_including_tax": price}
+        return price
 
     def get_price_tax_free(self):
         price_tax_free_tag = self.soup.find("th", string="Price (excl. tax)")
         price_tax_free = price_tax_free_tag.find_next().string
-        return {"price_excluding_tax": price_tax_free}
+        return price_tax_free
 
     def get_availability(self):
         availability_tag = self.soup.find("th", string="Availability")
         availability = availability_tag.find_next().string
-        return {"number_available": availability}
+        return availability
 
     def get_description(self):
         description_tag = self.soup.find(id="product_description")
@@ -45,13 +44,13 @@ class Book:
             description = description_tag.find_next("p").string
         else:
             description = ""
-        return {"product_description": description}
+        return description
 
     def get_category(self):
         category_parent_tag = self.soup.find("ul")
         category_tag = category_parent_tag.find_all_next("a")[-1]
         category = category_tag.string
-        return {"category": category}
+        return category
 
     def get_review_rating(self):
         review_tag = self.soup.find("p", class_=re.compile('star-rating'))
@@ -62,24 +61,24 @@ class Book:
                        "Four": 4,
                        "Five": 5}
         review = review_dict[review_string]
-        return {"review_rating": review}
+        return review
 
     def get_image_url(self):
         image_url_tag = self.soup.find("img")
         image_relative_url = image_url_tag['src']
         image_url = urljoin(self.url, image_relative_url)
-        return {"image_url": image_url}
+        return image_url
 
     def get_informations(self):
-        informations = {}
-        informations.update(self.get_url())
-        informations.update(self.get_upc())
-        informations.update(self.get_title())
-        informations.update(self.get_price())
-        informations.update(self.get_price_tax_free())
-        informations.update(self.get_availability())
-        informations.update(self.get_description())
-        informations.update(self.get_category())
-        informations.update(self.get_review_rating())
-        informations.update(self.get_image_url())
-        return informations
+        return {
+            'product_page_url': self.url,
+            'universal_product_code (upc)': self.get_upc,
+            'title': self.get_title,
+            'price_including_tax': self.get_price,
+            'price_excluding_tax': self.get_price_tax_free,
+            'number_available': self.get_availability,
+            'product_description': self.get_description,
+            'category': self.get_category,
+            'review_rating': self.get_review_rating,
+            'image_url': self.get_image_url
+        }
